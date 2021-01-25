31840552972498b7e66901427ae4750e562398f4
Marcos Adriano <marcosadriano740@gmail.com>
2021-01-25 17:10:13 -0300

    Alterando background image e o título e descrição do quiz

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/marcosadriano05" />
     </QuizBackground>
   );
 }
