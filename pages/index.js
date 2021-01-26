53f46be6f75237043a99879b226737b64527714d
CarlosZanao <carloszanao@gmail.com>
2021-01-25 23:44:46 -0300

    Add link Do projeto

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/CarlosZanao/aluraquiz-AmongUs.git" />
     </QuizBackground>
   );
 }
