ac5016236ee4b0d639152096a72a407b21c33c23
Emerson Pereira da Silva <emerson.pereirada@compasso.com.br>
2021-01-26 01:13:10 -0300

    Add repo git  and configs

--- pages/contribuidores/index.js
+++ pages/contribuidores/index.js
@@ -80,7 +80,7 @@ export default function ContributorsPage({ contributors }) {
         </div>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/eps364" />
     </QuizBackground>
   );
 }
