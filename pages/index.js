46f2105b2180f0293c57dbfa1a8be48129785200
Flavio Silva <contatoflaviodev@gmail.com>
2021-01-26 10:08:47 -0300

    Corrigindo espaço no botão entre o nome e 'Jogar'

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import React from "react";
+import styled from "styled-components";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizLogo from "../src/components/QuizLogo";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -30,7 +30,7 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState("");
 
   return (
     <QuizBackground backgroundImage={db.bg}>
@@ -44,11 +44,12 @@ export default function Home() {
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
-              router.push(`/quiz?name=${name}`);
-              console.log('Fazendo uma submissão por meio do react');
-            }}
+            <form
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log("Fazendo uma submissão por meio do react");
+              }}
             >
               <input
                 onChange={function (infosDoEvento) {
@@ -60,8 +61,7 @@ export default function Home() {
                 placeholder="Diz ai seu nome"
               />
               <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
+                Jogar {name}
               </button>
             </form>
           </Widget.Content>
