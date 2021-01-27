8fea8ede62959694045404973979b71c4dd867f9
Carlos Zanão <39104280+CarlosZanao@users.noreply.github.com>
2021-01-27 14:52:24 -0300

    Update index.js
    Resolução de conflitos

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/CarlosZanao/aluraquiz-AmongUs.git" />
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
     </QuizBackground>
   );
 }
