4ff0c7f90553120bebc4affd2f3870e3ccffafec
Marcos Adriano <marcosadriano740@gmail.com>
2021-01-27 19:56:00 -0300

    Criação do quizcontainer e tela de carregamento na page quiz

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,15 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import styled from 'styled-components'
 
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
+import QuizContainer from '../src/components/QuizContainer'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import Input from '../src/components/Input'
 import Button from '../src/components/Button'
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const [name, setName] = useState('')
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,28 @@
-export default function Quiz () {
+import db from '../db.json'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import Widget from '../src/components/Widget'
+
+function LoadingWidget() {
     return (
-        <div>
-            Olá
-        </div>
+      <Widget>
+        <Widget.Header>
+          Carregando...
+        </Widget.Header>
+  
+        <Widget.Content>
+          [Desafio do Loading]
+        </Widget.Content>
+      </Widget>
+    );
+}
+
+export default function QuizPage () {
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <LoadingWidget />
+            </QuizContainer>
+        </QuizBackground>
     )
 }
