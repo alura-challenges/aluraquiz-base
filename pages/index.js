eef50259cc2488a7c917776fdf62aa62f58a2fa3
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2021-01-27 15:31:35 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,7 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
+import { useRouter } from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -25,6 +19,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -35,6 +32,26 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+        <form onSubmit={function (infosDoEvento) {
+          infosDoEvento.preventDefault();
+          router.push(`/quiz?name=${name}`);
+          console.log('Fazendo uma submissão por meio do react');
+        }}
+        >
+          <input
+            onChange={function (infosDoEvento) {
+              console.log(infosDoEvento.target.value);
+              // State
+              // name = infosDoEvento.target.value;
+              setName(infosDoEvento.target.value);
+            }}
+            placeholder="Diz ai seu nome"
+          />
+          <button type="submit" disabled={name.length === 0}>
+            Jogar
+          </button>
+        </form>
+
           </Widget.Content>
         </Widget>
 
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
-export default function PudimComBatata() {
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function QuizAguardando() {
+  let search = window.location.search;
+  let params = new URLSearchParams(search);
+  let name = params.get('name');
+ 
   return (
-    <div>
-      Página de quiz
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+             Ainda em construção, {name}.
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
   );
 }
