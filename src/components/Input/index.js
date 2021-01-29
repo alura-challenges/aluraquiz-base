f33b6b402a32dba31ead451052e64d08b6fbc5f2
bentoco <hi.bentoco@gmail.com>
2021-01-29 18:45:45 -0300

    add folk's quiz

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const InputBase = styled.input`
-  width: 100%;
-  padding: 15px;
-  font-size: 14px;
-  text-align: center;
-  font-family: 'Sofadi One', cursive;
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  color: ${({ theme }) => theme.colors.contrastText};
-  background-color: ${({ theme }) => theme.colors.mainBg};
-  border-radius: ${({ theme }) => theme.borderRadius};
-  outline: 0;
-  margin-bottom: 25px;
-  ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-    color: ${({ theme }) => theme.colors.contrastText}DD;
-    opacity: 1; /* Firefox */
-  }
-
-`;
+    width: 100%;
+    padding: 15px;
+    font-size: 12px;
+    text-align: center;
+    font-family: 'Sofadi One', cursive;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.contrastText};
+    background-color: ${({ theme }) => theme.colors.mainBg};
+    border-radius: ${({ theme }) => theme.borderRadius};
+    outline: 0;
+    margin-bottom: 25px;
+    ::placeholder {
+        /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: ${({ theme }) => theme.colors.primary};
+        opacity: 1; /* Firefox */
+    }
+`
 
 export default function Input({ onChange, placeholder, ...props }) {
     return (
