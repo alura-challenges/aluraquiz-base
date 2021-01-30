4c43cb39a6bcb923e056c73a31a985798dc238cc
Marcos Adriano <marcosadriano740@gmail.com>
2021-01-30 00:30:47 -0300

    Criação api db

---
+++ pages/api/db.js
@@ -0,0 +1,14 @@
+import db from '../../db.json'
+
+export default function dbHandler(request, response) {
+    if(request.method === 'OPTIONS') {
+        response.status(200).end()
+        return
+    }
+
+    response.setHeader('Access-Control-Allow-Credentials', true)
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    
+    response.json(db)
+}
