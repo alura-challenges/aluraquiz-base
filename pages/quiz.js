e85e400c246b95aa6d832add4186484c46e1ddc1
Álvaro Patrício <alvaropatricio61@gmail.com>
2021-01-31 12:39:22 -0300

    Quiz query style and Quiz page edited

--- pages/quiz.js
+++ pages/quiz.js
@@ -10,25 +10,20 @@ import Button from '../src/components/Button';
 function LoadingWidget() {
   return (
     <Widget>
-      <Widget.Header>
-        Carregando...
-      </Widget.Header>
+      <Widget.Header>Carregando...</Widget.Header>
 
-      <Widget.Content>
-        [Desafio do Loading]
-      </Widget.Content>
+      <Widget.Content>[Desafio do Loading]</Widget.Content>
     </Widget>
   );
 }
 
-function QuestionWidget({ question }) {
+function QuestionWidget({ question, questionIndex, totalQuestions }) {
+  const questionId = `question__${questionIndex}`;
   return (
     <Widget>
       <Widget.Header>
         {/* <BackLinkArrow href="/" /> */}
-        <h3>
-          {`Pergunta 1 de ${db.questions.length}`}
-        </h3>
+        <h3>{`Pergunta ${questionIndex + 1} de ${totalQuestions}`}</h3>
       </Widget.Header>
 
       <img
@@ -41,26 +36,48 @@ function QuestionWidget({ question }) {
         src="https://tibiawiki.com.br/images/8/86/WinterUp5.png"
       />
       <Widget.Content>
-        <h2>
-          {question.title}
-        </h2>
-        <p>
-          {question.description}
-        </p>
-        <Button type="submit">
-          Confirmar
-        </Button>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+
+        <form>
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `Alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  // style={{ display: 'none'}}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button type="submit">Confirmar</Button>
+        </form>
       </Widget.Content>
     </Widget>
   );
 }
 
 export default function quizPage() {
-  const question = db.questions[0];
+  const totalQuestions = db.questions.length;
+  const questionIndex = 0;
+  const question = db.questions[questionIndex];
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <QuestionWidget question={question} />
+        <QuestionWidget
+          question={question}
+          totalQuestions={totalQuestions}
+          questionIndex={questionIndex}
+        />
         <LoadingWidget />
         <QuizLogo />
       </QuizContainer>
