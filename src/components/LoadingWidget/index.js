577a9af737d4b782714460a277618a832539051c
Gabriel Pereira <gabriel9938@gmail.com>
2021-01-31 11:35:12 -0300

    feat: Adicionando loading

--- src/components/LoadingWidget/index.js
+++ src/components/LoadingWidget/index.js
@@ -1,16 +1,46 @@
+import styled from 'styled-components';
 import Widget from '../../components/Widget'
+
+const Skeleton = styled.div`
+  background:linear-gradient( -90deg, #E0961F, #9E6A16,#E0961F);
+  background-size: 400% 400%;
+  border-radius: 8px;
+  animation: shimmer 1.2s ease-in-out infinite;
+  height: 20px;
+  width: 100%;
+
+  @keyframes shimmer{
+    0% {
+      background-position: 0% 0%;
+    }
+    100% {
+      background-position: -135% 0%;
+    }
+  }
+
+  & + & {
+    margin-top: 5px;
+    height: 40px;
+  }
+  
+`;
 
 export default function LoadingWidget() {
 
   return (
     <Widget>
       <Widget.Header>
-        <h2>
-          Loading...
-        </h2>
+        <Skeleton />
       </Widget.Header>
       <Widget.Content>
-        <p>Tenha paciência</p>
+        <Skeleton />
+        <Skeleton style= {{ height: '20px'}}/>
+        <Skeleton style={{marginTop: '25px'}}/>
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton style={{marginTop: '25px'}}/>
       </Widget.Content>
     </Widget>
   );
